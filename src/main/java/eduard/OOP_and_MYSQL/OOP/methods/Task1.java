d6d3af6783bf0e7ee384537c871ab3c1746e84fc
eduard <eduard.synohub@gmail.com>
2026-01-03 20:43:20 +0100

    OOP (methods)

---
+++ src/main/java/eduard/OOP_and_MYSQL/OOP/methods/Task1.java
@@ -0,0 +1,11 @@
+package eduard.OOP_and_MYSQL.OOP.methods;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Product banan = new Product();
+        System.out.println(banan.getName());
+        banan.setName("Banan");
+        System.out.println(banan.getName());
+        System.out.println(banan);
+    }
+}
---
+++ src/main/java/eduard/OOP_and_MYSQL/OOP/methods/Task3.java
@@ -0,0 +1,11 @@
+package eduard.OOP_and_MYSQL.OOP.methods;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Car honda = new Car();
+        honda.setBrand("Honda");
+        honda.setModel("Civic");
+        honda.setPrice(5000.0);
+        System.out.println(honda);
+    }
+}
