14e3f4550bc3b63b0945ac7c7f7c41617ccdb647
eduard <eduard.synohub@gmail.com>
2026-01-07 23:33:50 +0100

    OOP (advanced OOP)

--- src/main/java/eduard/OOP_and_MYSQL/OOP/advancedOOP/ShoppingCart.java
+++ src/main/java/eduard/OOP_and_MYSQL/OOP/advancedOOP/ShoppingCart.java
@@ -10,47 +10,88 @@ public class ShoppingCart {
     }
 
     public void addProduct(Product product, int quantity) {
-        if (cartItems.length == 0) {
-            cartItems = Arrays.copyOf(cartItems, cartItems.length + 1);
-            cartItems[cartItems.length - 1] = new CartItem(product, quantity);
-        } else {
-            for (int i = 0; i < cartItems.length; i++) {
-                if (!cartItems[i].getProduct().equals(product)) {
-                    cartItems = Arrays.copyOf(cartItems, cartItems.length + 1);
-                    cartItems[cartItems.length - 1] = new CartItem(product, quantity);
-                } else {
-                    int q = cartItems[i].getQuantity();
-                    cartItems[i].setQuantity(q + quantity);
-                }
+        checkQuantity(quantity);
+
+        for (int i = 0; i < cartItems.length; i++) {
+            if (cartItems[i].getProduct().equals(product)) {
+                int q = cartItems[i].getQuantity();
+                cartItems[i].setQuantity(q + quantity);
+                return;
             }
         }
+
+        cartItems = Arrays.copyOf(cartItems, cartItems.length + 1);
+        cartItems[cartItems.length - 1] = new CartItem(product, quantity);
     }
 
     public void removeProduct(Product product) {
-
+        CartItem[] toRemoveItem = new CartItem[0];
+        for (CartItem cartItem : cartItems) {
+            if (!cartItem.getProduct().equals(product)) {
+                toRemoveItem = Arrays.copyOf(toRemoveItem, toRemoveItem.length + 1);
+                toRemoveItem[toRemoveItem.length - 1] = cartItem;
+            }
+        }
+        cartItems = toRemoveItem;
     }
 
     public void updateProduct(Product product, int quantity) {
-        for (CartItem cartItem : cartItems) {
-            if (cartItem.getProduct().equals(product)) {
-                int q = cartItem.getQuantity();
-                cartItem.setQuantity(q + quantity);
-            } else {
-                addProduct(product, quantity);
+        checkQuantity(quantity);
+            for (int i = 0; i < cartItems.length; i++) {
+                if (cartItems[i].getProduct().getId() == product.getId()) {
+                    cartItems[i] = new CartItem(product, quantity);
+                }
             }
-        }
+            if (quantity == 0) {
+                removeProduct(product);
+            }
     }
 
     public int getTotalQuantity() {
-        return 0;
+        int totalQuantity = 0;
+
+        for (CartItem cartItem : cartItems) {
+            totalQuantity += cartItem.getQuantity();
+        }
+
+        return totalQuantity;
     }
 
     public double getTotalSum() {
-        return 0.0;
+        double sum = 0.0;
+        for (CartItem cartItem : cartItems) {
+            sum += (cartItem.getProduct().getPrice() * cartItem.getQuantity());
+        }
+        return sum;
     }
 
     public void printReceipt() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i  = 0; i < cartItems.length; i++) {
+            double sum = cartItems[i].getQuantity() * cartItems[i].getProduct().getPrice();
+            stringBuilder
+                    .append(i + 1)
+                    .append(": ")
+                    .append(cartItems[i].getProduct().getName())
+                    .append(" ")
+                    .append(cartItems[i].getProduct().getId())
+                    .append(", ")
+                    .append(cartItems[i].getQuantity())
+                    .append("x")
+                    .append(cartItems[i].getProduct().getPrice())
+                    .append(" = ")
+                    .append(sum)
+                    .append("\n");
+        }
+        System.out.print(stringBuilder);
+        System.out.println("---------------------------------------");
+        System.out.println("Razem: " + getTotalSum());
+    }
 
+    protected void checkQuantity(int quantity) {
+        if (quantity < 0) {
+            throw new RuntimeException("Quantity must be greater than 0");
+        }
     }
 
     @Override
